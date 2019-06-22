bf94032262bcbd30602e8a9e5d870f5b7b8e8405
ThatKawaiiSam <sam.sergi@outlook.com>
2019-06-22 16:10:29 +1000

    Added plugin constants.

---
+++ src/main/java/io/github/thatkawaiisam/hubselector/HubSelectorConstants.java
@@ -0,0 +1,8 @@
+package io.github.thatkawaiisam.hubselector;
+
+public class HubSelectorConstants {
+
+    public static int CONFIG_VERSION = 1;
+    public static String PACKAGE_NAME = "io.github.thatkawaiisam.hubselector";
+
+}
