c991c1f99bf89b02fa8cd7ad580bccedd211a85f
ThatKawaiiSam <sam.sergi@outlook.com>
2019-06-22 16:10:10 +1000

    Added Language, Configuration, and Command Handlers.

--- src/main/java/io/github/thatkawaiisam/hubselector/HubSelectorPlugin.java
+++ src/main/java/io/github/thatkawaiisam/hubselector/HubSelectorPlugin.java
@@ -3,6 +3,8 @@ package io.github.thatkawaiisam.hubselector;
 import io.github.plugintemplate.PluginTemplate;
 import io.github.plugintemplate.handler.Handler;
 import io.github.plugintemplate.handler.impl.CommandHandler;
+import io.github.plugintemplate.handler.impl.ConfigHandler;
+import io.github.plugintemplate.handler.impl.LanguageHandler;
 import io.github.thatkawaiisam.menus.MenuManager;
 import lombok.Getter;
 import org.bukkit.Bukkit;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 @Getter
 public class HubSelectorPlugin extends PluginTemplate {
 
+    //Instance
     @Getter private static HubSelectorPlugin instance;
 
     private MenuManager menuManager;
@@ -27,9 +30,10 @@ public class HubSelectorPlugin extends PluginTemplate {
 
         if (getHandlers() == null) {
             setHandlers(new ArrayList<>());
-            getHandlers().add(new CommandHandler("io.github.thatkawaiisam.hubselector.commands", this));
+            getHandlers().add(new ConfigHandler(HubSelectorConstants.CONFIG_VERSION, this));
+            getHandlers().add(new LanguageHandler(this));
+            getHandlers().add(new CommandHandler(HubSelectorConstants.PACKAGE_NAME + ".commands", this));
             getHandlers().add(new Handler("channel", false, this) {
-
                 Messenger messenger = Bukkit.getServer().getMessenger();
 
                 @Override
@@ -50,7 +54,6 @@ public class HubSelectorPlugin extends PluginTemplate {
     @Override
     public void onDisable() {
         menuManager.cleanup();
-
         getHandlers().forEach(Handler::disable);
     }
 }
